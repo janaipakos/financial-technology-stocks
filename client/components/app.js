86a2b724e1958273c5ff4f3c28050c848827794a
AND <janaipakos@gmail.com>
2016-04-22 16:48:30 -0400

    Updates

--- client/components/app.js
+++ client/components/app.js
@@ -12,6 +12,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var angular2_1 = require('angular2/angular2');
 var router_1 = require('angular2/router');
 var dashboard_1 = require('./dashboard');
+var clinton_1 = require('./clinton');
 var manage_1 = require('./manage');
 var App = (function () {
     function App() {
@@ -19,6 +20,7 @@ var App = (function () {
     App = __decorate([
         router_1.RouteConfig([
             { path: '/', as: 'Dashboard', component: dashboard_1.Dashboard },
+            { path: '/clinton', as: 'Clinton', component: clinton_1.Clinton },
             { path: '/manage', as: 'Manage', component: manage_1.Manage }
         ]),
         angular2_1.Component({
@@ -26,7 +28,7 @@ var App = (function () {
         }),
         angular2_1.View({
             directives: [router_1.RouterOutlet, router_1.RouterLink],
-            template: "\n  <div class=\"mdl-layout mdl-js-layout mdl-layout--fixed-header\">\n    <header class=\"mdl-layout__header\">\n      <div class=\"mdl-layout__header-row\">\n        <span class=\"mdl-layout-title\">Stock Tracker</span>\n        <div class=\"mdl-layout-spacer\"></div>\n        <nav class=\"mdl-navigation mdl-layout--large-screen-only\">\n          <a class=\"mdl-navigation__link\" [router-link]=\"['/Dashboard']\">Dashboard</a>\n          <a class=\"mdl-navigation__link\" [router-link]=\"['/Manage']\">Manage</a>\n        </nav>\n      </div>\n    </header>\n    <main class=\"mdl-layout__content\" style=\"padding: 20px;\">\n      <router-outlet></router-outlet>\n    </main>\n  </div>\n  "
+            template: "\n  <div class=\"mdl-layout mdl-js-layout mdl-layout--fixed-header\">\n    <header class=\"mdl-layout__header\">\n      <div class=\"mdl-layout__header-row\">\n        <span class=\"mdl-layout-title\">Stock Tracker</span>\n        <div class=\"mdl-layout-spacer\"></div>\n        <nav class=\"mdl-navigation mdl-layout--large-screen-only\">\n          <a class=\"mdl-navigation__link\" [router-link]=\"['/Dashboard']\">Dashboard</a>\n          <a class=\"mdl-navigation__link\" [router-link]=\"['/Clinton]\">Clinton</a>\n          <a class=\"mdl-navigation__link\" [router-link]=\"['/Manage']\">Manage</a>\n        </nav>\n      </div>\n    </header>\n    <main class=\"mdl-layout__content\" style=\"padding: 20px;\">\n      <router-outlet></router-outlet>\n    </main>\n  </div>\n  "
         }), 
         __metadata('design:paramtypes', [])
     ], App);
